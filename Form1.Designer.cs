5cb10b452962ae806cc0dce16acd2c8dab8b9d99
Faulin <39439472+FaulinV@users.noreply.github.com>
2025-11-11 22:51:47 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,385 @@
+﻿namespace rekenmachineProject
+{
+    partial class Rekenmachine
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Rekenmachine));
+            this.lblDisplay = new Sunny.UI.UILabel();
+            this.uiBtnDiv = new Sunny.UI.UIButton();
+            this.uiButton1 = new Sunny.UI.UIButton();
+            this.uiBtnBk = new Sunny.UI.UIButton();
+            this.uiBtnMul = new Sunny.UI.UIButton();
+            this.uiBtnSub = new Sunny.UI.UIButton();
+            this.uiBtn8 = new Sunny.UI.UIButton();
+            this.uiBtn7 = new Sunny.UI.UIButton();
+            this.uiBtn9 = new Sunny.UI.UIButton();
+            this.uiBtn5 = new Sunny.UI.UIButton();
+            this.uiBtn4 = new Sunny.UI.UIButton();
+            this.uiBtn6 = new Sunny.UI.UIButton();
+            this.uiBtnAdd = new Sunny.UI.UIButton();
+            this.uiBtn2 = new Sunny.UI.UIButton();
+            this.uiBtn1 = new Sunny.UI.UIButton();
+            this.uiBtn3 = new Sunny.UI.UIButton();
+            this.uiBtnEq = new Sunny.UI.UIButton();
+            this.uiBtn0 = new Sunny.UI.UIButton();
+            this.uiBtnAC = new Sunny.UI.UIButton();
+            this.uiBtnDot = new Sunny.UI.UIButton();
+            this.uiLabel1 = new Sunny.UI.UILabel();
+            this.SuspendLayout();
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.lblDisplay.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.lblDisplay.Location = new System.Drawing.Point(0, 35);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(246, 60);
+            this.lblDisplay.TabIndex = 0;
+            this.lblDisplay.Text = "0";
+            this.lblDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // uiBtnDiv
+            // 
+            this.uiBtnDiv.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiBtnDiv.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.uiBtnDiv.Location = new System.Drawing.Point(125, 110);
+            this.uiBtnDiv.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiBtnDiv.Name = "uiBtnDiv";
+            this.uiBtnDiv.Radius = 20;
+            this.uiBtnDiv.Size = new System.Drawing.Size(50, 50);
+            this.uiBtnDiv.TabIndex = 3;
+            this.uiBtnDiv.Text = "÷";
+            this.uiBtnDiv.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            // 
+            // uiButton1
+            // 
+            this.uiButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.uiButton1.Location = new System.Drawing.Point(13, 110);
+            this.uiButton1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiButton1.Name = "uiButton1";
+            this.uiButton1.Radius = 20;
+            this.uiButton1.Size = new System.Drawing.Size(50, 50);
+            this.uiButton1.TabIndex = 4;
+            this.uiButton1.Text = "C";
+            this.uiButton1.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            // 
+            // uiBtnBk
+            // 
+            this.uiBtnBk.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiBtnBk.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.uiBtnBk.Location = new System.Drawing.Point(69, 110);
+            this.uiBtnBk.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiBtnBk.Name = "uiBtnBk";
+            this.uiBtnBk.Radius = 20;
+            this.uiBtnBk.Size = new System.Drawing.Size(50, 50);
+            this.uiBtnBk.TabIndex = 5;
+            this.uiBtnBk.Text = "⌫";
+            this.uiBtnBk.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            // 
+            // uiBtnMul
+            // 
+            this.uiBtnMul.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiBtnMul.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.uiBtnMul.Location = new System.Drawing.Point(181, 110);
+            this.uiBtnMul.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiBtnMul.Name = "uiBtnMul";
+            this.uiBtnMul.Radius = 20;
+            this.uiBtnMul.Size = new System.Drawing.Size(50, 50);
+            this.uiBtnMul.TabIndex = 6;
+            this.uiBtnMul.Text = "×";
+            this.uiBtnMul.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            // 
+            // uiBtnSub
+            // 
+            this.uiBtnSub.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiBtnSub.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.uiBtnSub.Location = new System.Drawing.Point(181, 166);
+            this.uiBtnSub.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiBtnSub.Name = "uiBtnSub";
+            this.uiBtnSub.Radius = 20;
+            this.uiBtnSub.Size = new System.Drawing.Size(50, 50);
+            this.uiBtnSub.TabIndex = 18;
+            this.uiBtnSub.Text = "-";
+            this.uiBtnSub.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            // 
+            // uiBtn8
+            // 
+            this.uiBtn8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiBtn8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.uiBtn8.Location = new System.Drawing.Point(69, 166);
+            this.uiBtn8.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiBtn8.Name = "uiBtn8";
+            this.uiBtn8.Radius = 20;
+            this.uiBtn8.Size = new System.Drawing.Size(50, 50);
+            this.uiBtn8.TabIndex = 17;
+            this.uiBtn8.Text = "8";
+            this.uiBtn8.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            // 
+            // uiBtn7
+            // 
+            this.uiBtn7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiBtn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.uiBtn7.Location = new System.Drawing.Point(13, 166);
+            this.uiBtn7.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiBtn7.Name = "uiBtn7";
+            this.uiBtn7.Radius = 20;
+            this.uiBtn7.Size = new System.Drawing.Size(50, 50);
+            this.uiBtn7.TabIndex = 16;
+            this.uiBtn7.Text = "7";
+            this.uiBtn7.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            // 
+            // uiBtn9
+            // 
+            this.uiBtn9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiBtn9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.uiBtn9.Location = new System.Drawing.Point(125, 166);
+            this.uiBtn9.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiBtn9.Name = "uiBtn9";
+            this.uiBtn9.Radius = 20;
+            this.uiBtn9.Size = new System.Drawing.Size(50, 50);
+            this.uiBtn9.TabIndex = 15;
+            this.uiBtn9.Text = "9";
+            this.uiBtn9.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            // 
+            // uiBtn5
+            // 
+            this.uiBtn5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiBtn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.uiBtn5.Location = new System.Drawing.Point(69, 222);
+            this.uiBtn5.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiBtn5.Name = "uiBtn5";
+            this.uiBtn5.Radius = 20;
+            this.uiBtn5.Size = new System.Drawing.Size(50, 50);
+            this.uiBtn5.TabIndex = 21;
+            this.uiBtn5.Text = "5";
+            this.uiBtn5.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            // 
+            // uiBtn4
+            // 
+            this.uiBtn4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiBtn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.uiBtn4.Location = new System.Drawing.Point(13, 222);
+            this.uiBtn4.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiBtn4.Name = "uiBtn4";
+            this.uiBtn4.Radius = 20;
+            this.uiBtn4.Size = new System.Drawing.Size(50, 50);
+            this.uiBtn4.TabIndex = 20;
+            this.uiBtn4.Text = "4";
+            this.uiBtn4.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            // 
+            // uiBtn6
+            // 
+            this.uiBtn6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiBtn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.uiBtn6.Location = new System.Drawing.Point(125, 222);
+            this.uiBtn6.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiBtn6.Name = "uiBtn6";
+            this.uiBtn6.Radius = 20;
+            this.uiBtn6.Size = new System.Drawing.Size(50, 50);
+            this.uiBtn6.TabIndex = 19;
+            this.uiBtn6.Text = "6";
+            this.uiBtn6.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            // 
+            // uiBtnAdd
+            // 
+            this.uiBtnAdd.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiBtnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.uiBtnAdd.Location = new System.Drawing.Point(181, 222);
+            this.uiBtnAdd.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiBtnAdd.Name = "uiBtnAdd";
+            this.uiBtnAdd.Radius = 20;
+            this.uiBtnAdd.Size = new System.Drawing.Size(50, 84);
+            this.uiBtnAdd.TabIndex = 26;
+            this.uiBtnAdd.Text = "+";
+            this.uiBtnAdd.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            // 
+            // uiBtn2
+            // 
+            this.uiBtn2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiBtn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.uiBtn2.Location = new System.Drawing.Point(69, 278);
+            this.uiBtn2.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiBtn2.Name = "uiBtn2";
+            this.uiBtn2.Radius = 20;
+            this.uiBtn2.Size = new System.Drawing.Size(50, 50);
+            this.uiBtn2.TabIndex = 25;
+            this.uiBtn2.Text = "2";
+            this.uiBtn2.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            // 
+            // uiBtn1
+            // 
+            this.uiBtn1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiBtn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.uiBtn1.Location = new System.Drawing.Point(13, 278);
+            this.uiBtn1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiBtn1.Name = "uiBtn1";
+            this.uiBtn1.Radius = 20;
+            this.uiBtn1.Size = new System.Drawing.Size(50, 50);
+            this.uiBtn1.TabIndex = 24;
+            this.uiBtn1.Text = "1";
+            this.uiBtn1.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            // 
+            // uiBtn3
+            // 
+            this.uiBtn3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiBtn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.uiBtn3.Location = new System.Drawing.Point(125, 278);
+            this.uiBtn3.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiBtn3.Name = "uiBtn3";
+            this.uiBtn3.Radius = 20;
+            this.uiBtn3.Size = new System.Drawing.Size(50, 50);
+            this.uiBtn3.TabIndex = 23;
+            this.uiBtn3.Text = "3";
+            this.uiBtn3.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            // 
+            // uiBtnEq
+            // 
+            this.uiBtnEq.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiBtnEq.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.uiBtnEq.Location = new System.Drawing.Point(181, 312);
+            this.uiBtnEq.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiBtnEq.Name = "uiBtnEq";
+            this.uiBtnEq.Radius = 20;
+            this.uiBtnEq.Size = new System.Drawing.Size(50, 72);
+            this.uiBtnEq.TabIndex = 30;
+            this.uiBtnEq.Text = "=";
+            this.uiBtnEq.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            // 
+            // uiBtn0
+            // 
+            this.uiBtn0.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiBtn0.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.uiBtn0.Location = new System.Drawing.Point(69, 334);
+            this.uiBtn0.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiBtn0.Name = "uiBtn0";
+            this.uiBtn0.Radius = 20;
+            this.uiBtn0.Size = new System.Drawing.Size(50, 50);
+            this.uiBtn0.TabIndex = 29;
+            this.uiBtn0.Text = "0";
+            this.uiBtn0.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            // 
+            // uiBtnAC
+            // 
+            this.uiBtnAC.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiBtnAC.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.uiBtnAC.Location = new System.Drawing.Point(13, 334);
+            this.uiBtnAC.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiBtnAC.Name = "uiBtnAC";
+            this.uiBtnAC.Radius = 20;
+            this.uiBtnAC.Size = new System.Drawing.Size(50, 50);
+            this.uiBtnAC.TabIndex = 28;
+            this.uiBtnAC.Text = "AC";
+            this.uiBtnAC.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            // 
+            // uiBtnDot
+            // 
+            this.uiBtnDot.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiBtnDot.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.uiBtnDot.Location = new System.Drawing.Point(125, 334);
+            this.uiBtnDot.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiBtnDot.Name = "uiBtnDot";
+            this.uiBtnDot.Radius = 20;
+            this.uiBtnDot.Size = new System.Drawing.Size(50, 50);
+            this.uiBtnDot.TabIndex = 27;
+            this.uiBtnDot.Text = ".";
+            this.uiBtnDot.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.uiBtnDot.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            // 
+            // uiLabel1
+            // 
+            this.uiLabel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.uiLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uiLabel1.ForeColor = System.Drawing.Color.Gray;
+            this.uiLabel1.Location = new System.Drawing.Point(0, 391);
+            this.uiLabel1.Name = "uiLabel1";
+            this.uiLabel1.Size = new System.Drawing.Size(246, 23);
+            this.uiLabel1.TabIndex = 31;
+            this.uiLabel1.Text = "Gemaakt door: Zhan Kornilov";
+            // 
+            // Rekenmachine
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(246, 414);
+            this.Controls.Add(this.uiLabel1);
+            this.Controls.Add(this.uiBtnEq);
+            this.Controls.Add(this.uiBtn0);
+            this.Controls.Add(this.uiBtnAC);
+            this.Controls.Add(this.uiBtnDot);
+            this.Controls.Add(this.uiBtnAdd);
+            this.Controls.Add(this.uiBtn2);
+            this.Controls.Add(this.uiBtn1);
+            this.Controls.Add(this.uiBtn3);
+            this.Controls.Add(this.uiBtn5);
+            this.Controls.Add(this.uiBtn4);
+            this.Controls.Add(this.uiBtn6);
+            this.Controls.Add(this.uiBtnSub);
+            this.Controls.Add(this.uiBtn8);
+            this.Controls.Add(this.uiBtn7);
+            this.Controls.Add(this.uiBtn9);
+            this.Controls.Add(this.uiBtnMul);
+            this.Controls.Add(this.uiBtnBk);
+            this.Controls.Add(this.uiButton1);
+            this.Controls.Add(this.uiBtnDiv);
+            this.Controls.Add(this.lblDisplay);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Rekenmachine";
+            this.Text = "Rekenmachine";
+            this.ZoomScaleRect = new System.Drawing.Rectangle(15, 15, 284, 456);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Sunny.UI.UILabel lblDisplay;
+        private Sunny.UI.UIButton uiBtnDiv;
+        private Sunny.UI.UIButton uiButton1;
+        private Sunny.UI.UIButton uiBtnBk;
+        private Sunny.UI.UIButton uiBtnMul;
+        private Sunny.UI.UIButton uiBtnSub;
+        private Sunny.UI.UIButton uiBtn8;
+        private Sunny.UI.UIButton uiBtn7;
+        private Sunny.UI.UIButton uiBtn9;
+        private Sunny.UI.UIButton uiBtn5;
+        private Sunny.UI.UIButton uiBtn4;
+        private Sunny.UI.UIButton uiBtn6;
+        private Sunny.UI.UIButton uiBtnAdd;
+        private Sunny.UI.UIButton uiBtn2;
+        private Sunny.UI.UIButton uiBtn1;
+        private Sunny.UI.UIButton uiBtn3;
+        private Sunny.UI.UIButton uiBtnEq;
+        private Sunny.UI.UIButton uiBtn0;
+        private Sunny.UI.UIButton uiBtnAC;
+        private Sunny.UI.UIButton uiBtnDot;
+        private Sunny.UI.UILabel uiLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Sunny.UI; // voor de mooie knoppies en dingentjes
+
+namespace rekenmachineProject
+{
+    public partial class Rekenmachine : UIForm
+    {
+        private string expr = "";
+        private bool justEvaluated = false;
+        private readonly string decSep;
+
+        public Rekenmachine()
+        {
+            InitializeComponent();
+
+            this.Style = UIStyle.Custom;
+            this.TitleHeight = 36;
+            this.TitleColor = Color.Blue;
+
+            decSep = CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+
+            lblDisplay.Text = "0";
+
+            WireAllButtons(this);
+        }
+
+        private void WireAllButtons(Control root)
+        {
+            foreach (Control c in root.Controls)
+            {
+                if (c is UIButton b)
+                {
+                    string t = (b.Text ?? "").Trim();
+
+                    if (t.Length == 1 && char.IsDigit(t[0])) b.Click += Digit_Click;
+                    else if (t == "," || t == ".") b.Click += Dot_Click;
+                    else if (t == "+" || t == "-" || t == "×" || t == "x" || t == "*" || t == "÷" || t == "/")
+                    { b.Tag = NormalizeOperator(t); b.Click += Operator_Click; }
+                    else if (t == "=") b.Click += Equals_Click;
+                    else if (t.Equals("AC", StringComparison.OrdinalIgnoreCase)) b.Click += AC_Click;
+                    else if (t.Equals("C", StringComparison.OrdinalIgnoreCase)) b.Click += C_Click;
+                    else if (t == "⌫" || t == "←" || t.Equals("Back", StringComparison.OrdinalIgnoreCase)) b.Click += Backspace_Click;
+                }
+                if (c.HasChildren) WireAllButtons(c);
+            }
+        }
+
+        private string NormalizeOperator(string t)
+        {
+            switch (t)
+            {
+                case "+": return "+";
+                case "-": return "-";
+                case "×":
+                case "x":
+                case "X":
+                case "*": return "×";
+                case "÷":
+                case "/": return "÷";
+                default: return null;
+            }
+        }
+
+        private void Digit_Click(object sender, EventArgs e)
+        {
+            var d = ((UIButton)sender).Text;
+
+            if (justEvaluated) { expr = ""; justEvaluated = false; }
+
+            if (expr == "0") expr = d;
+            else expr += d;
+
+            UpdateDisplay();
+        }
+
+        private void Dot_Click(object sender, EventArgs e)
+        {
+            if (justEvaluated) { expr = ""; justEvaluated = false; }
+
+            string lastNumber = GetLastNumberSegment();
+            if (lastNumber.Contains(decSep)) return;
+
+            if (expr.Length == 0 || IsLastCharOperator())
+                expr += "0" + decSep;
+            else
+                expr += decSep;
+
+            UpdateDisplay();
+        }
+
+        private void Operator_Click(object sender, EventArgs e)
+        {
+            var op = (string)(((UIButton)sender).Tag ?? NormalizeOperator(((UIButton)sender).Text.Trim()));
+            if (op == null) return;
+
+            justEvaluated = false;
+
+            if (expr.Length == 0)
+            {
+                if (op == "-") { expr = "-"; UpdateDisplay(); }
+                return;
+            }
+
+            if (IsLastCharOperator())
+            {
+                expr = expr.Substring(0, expr.Length - 1) + op;
+            }
+            else if (expr.EndsWith(decSep))
+            {
+                return;
+            }
+            else
+            {
+                expr += op;
+            }
+
+            UpdateDisplay();
+        }
+
+        private void Equals_Click(object sender, EventArgs e)
+        {
+            if (expr.Length == 0) return;
+            if (IsLastCharOperator() || expr.EndsWith(decSep)) return;
+
+            var ok = TryEvaluate(expr, out string result);
+            if (ok)
+            {
+                expr = result;
+                justEvaluated = true;
+                UpdateDisplay();
+            }
+            else
+            {
+                lblDisplay.Text = "Error";
+                justEvaluated = true;
+            }
+        }
+
+        private void AC_Click(object sender, EventArgs e)
+        {
+            expr = "";
+            justEvaluated = false;
+            UpdateDisplay();
+        }
+
+        private void C_Click(object sender, EventArgs e)
+        {
+            int cut = LastOperatorIndex();
+            expr = cut >= 0 ? expr.Substring(0, cut + 1) : "";
+            UpdateDisplay();
+        }
+
+        private void Backspace_Click(object sender, EventArgs e)
+        {
+            if (expr.Length == 0 || justEvaluated)
+            {
+                expr = "";
+                UpdateDisplay();
+                justEvaluated = false;
+                return;
+            }
+            expr = expr.Substring(0, expr.Length - 1);
+            UpdateDisplay();
+        }
+
+        private void UpdateDisplay()
+        {
+            lblDisplay.Text = expr.Length == 0 ? "0" : expr;
+        }
+
+        private bool IsLastCharOperator()
+        {
+            if (expr.Length == 0) return false;
+            char ch = expr[expr.Length - 1];
+            return ch == '+' || ch == '-' || ch == '×' || ch == '÷';
+        }
+
+        private int LastOperatorIndex()
+        {
+            int i1 = expr.LastIndexOf('+');
+            int i2 = expr.LastIndexOf('-');
+            int i3 = expr.LastIndexOf('×');
+            int i4 = expr.LastIndexOf('÷');
+            return Math.Max(Math.Max(i1, i2), Math.Max(i3, i4));
+        }
+
+        private string GetLastNumberSegment()
+        {
+            int cut = LastOperatorIndex();
+            return cut >= 0 ? expr.Substring(cut + 1) : expr;
+        }
+
+        private bool TryEvaluate(string input, out string result)
+        {
+            try
+            {
+                string s = input.Replace('×', '*').Replace('÷', '/');
+                if (decSep == ",") s = s.Replace(',', '.');
+
+                var dt = new DataTable();
+                dt.Locale = CultureInfo.InvariantCulture;
+                object val = dt.Compute(s, null);
+
+                string text = Convert.ToDouble(val, CultureInfo.InvariantCulture)
+                                  .ToString("G15", CultureInfo.CurrentCulture);
+
+                result = text;
+                return true;
+            }
+            catch
+            {
+                result = null;
+                return false;
+            }
+        }
+    }
+}
